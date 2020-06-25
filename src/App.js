8af25f0fe7ab20a99bf61bdad6a0b220aa8eebb9
Piotr Soliński <solinskip@icloud.com>
2020-06-25 13:35:33 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+class App extends Component {
+    render() {
+        return (
+            <SafeAreaView style={{flex: 1}}>
+                <Text>It's works!</Text>
+            </SafeAreaView>
+        );
+    }
+}
+
+export default App;
